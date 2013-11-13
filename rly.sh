d15d09da9168166b0cd941ffc7d6b3ac2173999c
Adam Timberlake <adam.timberlake@gmail.com>
2013-11-13 20:53:43 +0000

    Updated Shell script

--- rly.sh
+++ rly.sh
@@ -2,8 +2,23 @@
 # Relayed
 
 function begin {
-    echo "Relaying: $1:$2";
     node ./Relayed.js $1 $2
 }
 
-begin $1 $2
+HOST=$1
+
+if [[ -z "$HOST" ]]; then
+    echo "Relayed: Please specify the host..."
+    exit 2;
+fi
+
+if [ $2 ]; then
+    PORT=$2;
+else
+    PORT=80;
+fi
+
+TEXT="Relayed: Listening to \"${HOST}\" on port ${PORT}..."
+echo $TEXT
+
+begin $HOST $PORT
