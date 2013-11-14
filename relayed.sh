250aa7632979aac21824f523706c63ec65da2e60
Adam Timberlake <adam.timberlake@gmail.com>
2013-11-14 19:54:48 +0000

    Improved colours

--- relayed.sh
+++ relayed.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 # Relayed
 
+RCol='\x1b[0m'    # Text Reset
+
+# Regular           Bold                Underline           High Intensity      BoldHigh Intens     Background          High Intensity Backgrounds
+Bla='\x1b[0;30m';     BBla='\x1b[1;30m';    UBla='\x1b[4;30m';    IBla='\x1b[0;90m';    BIBla='\x1b[1;90m';   On_Bla='\x1b[40m';    On_IBla='\x1b[0;100m';
+Red='\x1b[0;31m';     BRed='\x1b[1;31m';    URed='\x1b[4;31m';    IRed='\x1b[0;91m';    BIRed='\x1b[1;91m';   On_Red='\x1b[41m';    On_IRed='\x1b[0;101m';
+Gre='\x1b[0;32m';     BGre='\x1b[1;32m';    UGre='\x1b[4;32m';    IGre='\x1b[0;92m';    BIGre='\x1b[1;92m';   On_Gre='\x1b[42m';    On_IGre='\x1b[0;102m';
+Yel='\x1b[0;33m';     BYel='\x1b[1;33m';    UYel='\x1b[4;33m';    IYel='\x1b[0;93m';    BIYel='\x1b[1;93m';   On_Yel='\x1b[43m';    On_IYel='\x1b[0;103m';
+Blu='\x1b[0;34m';     BBlu='\x1b[1;34m';    UBlu='\x1b[4;34m';    IBlu='\x1b[0;94m';    BIBlu='\x1b[1;94m';   On_Blu='\x1b[44m';    On_IBlu='\x1b[0;104m';
+Pur='\x1b[0;35m';     BPur='\x1b[1;35m';    UPur='\x1b[4;35m';    IPur='\x1b[0;95m';    BIPur='\x1b[1;95m';   On_Pur='\x1b[45m';    On_IPur='\x1b[0;105m';
+Cya='\x1b[0;36m';     BCya='\x1b[1;36m';    UCya='\x1b[4;36m';    ICya='\x1b[0;96m';    BICya='\x1b[1;96m';   On_Cya='\x1b[46m';    On_ICya='\x1b[0;106m';
+Whi='\x1b[0;37m';     BWhi='\x1b[1;37m';    UWhi='\x1b[4;37m';    IWhi='\x1b[0;97m';    BIWhi='\x1b[1;97m';   On_Whi='\x1b[47m';    On_IWhi='\x1b[0;107m';
+
 function begin {
     node ./relayed.js $1 $2
 }
 
 function help {
     echo ""
-    echo "Relayed - Convenient Shell/Node script for circumventing CORS issues when developing on localhost."
+    echo -e "${Bla}${On_Cya} Relayed ${RCol} - Convenient Shell/Node script for circumventing CORS issues when developing on localhost."
     echo ""
-    echo "    relayed http://www.yahoo.com/          - begin relaying to http://www.yahoo.com:80/"
-    echo "    relayed http://www.yahoo.com/ 8080     - begin relaying to http://www.yahoo.com:8080/"
+    echo -e "    ${IBla}${On_Whi} + ${RCol} http://www.yahoo.com/          - Begin relaying to http://www.yahoo.com:80/"
+    echo -e "    ${IBla}${On_Whi} + ${RCol} http://www.yahoo.com/ 8080     - Begin relaying to http://www.yahoo.com:8080/"
     echo ""
 }
 
@@ -27,22 +39,10 @@ else
     PORT=80;
 fi
 
-RCol='\e[0m'    # Text Reset
-
-# Regular           Bold                Underline           High Intensity      BoldHigh Intens     Background          High Intensity Backgrounds
-Bla='\e[0;30m';     BBla='\e[1;30m';    UBla='\e[4;30m';    IBla='\e[0;90m';    BIBla='\e[1;90m';   On_Bla='\e[40m';    On_IBla='\e[0;100m';
-Red='\e[0;31m';     BRed='\e[1;31m';    URed='\e[4;31m';    IRed='\e[0;91m';    BIRed='\e[1;91m';   On_Red='\e[41m';    On_IRed='\e[0;101m';
-Gre='\e[0;32m';     BGre='\e[1;32m';    UGre='\e[4;32m';    IGre='\e[0;92m';    BIGre='\e[1;92m';   On_Gre='\e[42m';    On_IGre='\e[0;102m';
-Yel='\e[0;33m';     BYel='\e[1;33m';    UYel='\e[4;33m';    IYel='\e[0;93m';    BIYel='\e[1;93m';   On_Yel='\e[43m';    On_IYel='\e[0;103m';
-Blu='\e[0;34m';     BBlu='\e[1;34m';    UBlu='\e[4;34m';    IBlu='\e[0;94m';    BIBlu='\e[1;94m';   On_Blu='\e[44m';    On_IBlu='\e[0;104m';
-Pur='\e[0;35m';     BPur='\e[1;35m';    UPur='\e[4;35m';    IPur='\e[0;95m';    BIPur='\e[1;95m';   On_Pur='\e[45m';    On_IPur='\e[0;105m';
-Cya='\e[0;36m';     BCya='\e[1;36m';    UCya='\e[4;36m';    ICya='\e[0;96m';    BICya='\e[1;96m';   On_Cya='\e[46m';    On_ICya='\e[0;106m';
-Whi='\e[0;37m';     BWhi='\e[1;37m';    UWhi='\e[4;37m';    IWhi='\e[0;97m';    BIWhi='\e[1;97m';   On_Whi='\e[47m';    On_IWhi='\e[0;107m';
-
 echo ""
-echo -e "${Blu}RELAYED${RCol} - Convenient Shell/Node script for circumventing CORS issues when developing on localhost."
+echo -e "${Bla}${On_Cya} Relayed ${RCol} - Convenient Shell/Node script for circumventing CORS issues when developing on localhost."
 echo ""
-echo "Listening to \"${HOST}\" on port ${PORT}..."
+echo -e "${Bla}${On_Gre} Listening to \"${HOST}\" on port ${PORT}... ${RCol}"
 echo ""
 
 begin $HOST $PORT
