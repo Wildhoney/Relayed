8636007d7094e09ef8d4da102a6ad08f7d3cd036
Adam Timberlake <adam.timberlake@gmail.com>
2014-03-04 23:08:55 +0000

    Converted to Node.js. Fixes #2

--- bin/relayed.js
+++ bin/relayed.js
@@ -1,75 +1,142 @@
 #!/usr/bin/env node
-(function Relayed($host, $port) {
+
+/**
+ * @module Relayed
+ * @author Adam Timberlake
+ */
+(function Main($host, $port) {
 
     "use strict";
 
+    // Usual includes.
     var http         = require('http'),
         url          = require('url'),
-        childProcess = require('child_process');
+        childProcess = require('child_process'),
+        args         = require('minimist')(process.argv.slice(2));
 
-    http.createServer(function createServer(request, response) {
+    /**
+     * @property Relayed
+     * @constructor
+     */
+    var Relayed = function Relayed() {
 
-        // Set all of the necessary headers for CORS support.
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        response.setHeader('Access-Control-Allow-Credentials', true);
+        // Initialise the server to listen for incoming requests.
+        http.createServer(this._initialiseServer.bind(this)).listen($port, $host);
+        
+        if (args.o) {
 
-        // Passed in variables from the Shell script.
-        var targetUrl  = process.argv[2],
-            targetPort = process.argv[3],
-            parsedUrl  = url.parse(targetUrl);
-
-        // Determine whether to use HTTP or HTTPS for the relay.
-        var transport = parsedUrl.protocol === 'https:' ? require('https') : require('http');
-
-        if (parsedUrl.protocol === 'https:') {
-
-            // Process all HTTPS requests irrespective of certificates!
-            process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+            // Open the browser to point to the relayed URL.
+            childProcess.spawn('open', ['http://' + $host + ':' + $port]);
 
         }
 
-        var headers  = request.headers;
-        headers.host = parsedUrl.host;
+    };
+
+    /**
+     * @property prototype
+     * @type {Object}
+     */
+    Relayed.prototype = {
 
         /**
-         * @property relayedRequest
-         * @type {Object}
+         * @constant DEFAULT_PORT
+         * @type {Number}
+         * @default 80
          */
-        var relayedRequest = transport.request({
+        DEFAULT_PORT: 80,
 
-            host:    headers.host,
-            port:    targetPort,
-            path:    request.url,
-            method:  request.method,
-            headers: headers
+        /**
+         * @method _initialiseServer
+         * @param request {Object}
+         * @param response {Object}
+         * @return {void}
+         * @private
+         */
+        _initialiseServer: function _initialiseServer(request, response) {
 
-        }, function(relayedResponse) {
+            var targetPort = args.p || this.DEFAULT_PORT,
+                parsedUrl  = url.parse(args.h);
 
-            response.writeHead(relayedResponse.statusCode, relayedResponse.headers);
+            var headers  = request.headers;
+            headers.host = parsedUrl.host;
 
-            relayedResponse.on('data', function onData(data) {
-                response.write(data);
+            /**
+             * @property relayedRequest
+             * @type {Object}
+             */
+            var relayedRequest = this._getTransportProtocol().request({
+
+                host:    headers.host,
+                port:    targetPort,
+                path:    request.url,
+                method:  request.method,
+                headers: headers
+
+            }, function(data) {
+
+                response.writeHead(data.statusCode, data.headers);
+
+                // When we receive data from the relayed request.
+                data.on('data', function onData(datum) {
+                    response.write(datum);
+                });
+
+                // When the connection has been closed.
+                data.on('end', function onEnd() {
+                    response.end();
+                });
+
             });
 
-            relayedResponse.on('end', function onEnd() {
-                response.end();
+            // We just need to write anything to complete the request.
+            request.on('data', function onData() {
+                relayedRequest.write();
             });
 
-        });
+            // When we close the connection entirely.
+            request.on('end', function onEnd() {
+                relayedRequest.end();
+            });
 
-        request.on('data', function onData(response) {
-            relayedRequest.write('ts');
-        });
+        },
 
-        request.on('end', function onEnd() {
-            relayedRequest.end();
-        });
+        /**
+         * Set all of the necessary headers for CORS support.
+         *
+         * @method _configureCrossDomain
+         * @param response {Object}
+         * @return {void}
+         * @private
+         */
+        _configureCrossDomain: function _configureCrossDomain(response) {
+            response.setHeader('Access-Control-Allow-Origin', '*');
+            response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            response.setHeader('Access-Control-Allow-Credentials', true);
+        },
 
-    }).listen($port, $host);
+        /**
+         * @method _getTransportProtocol
+         * @return {Object}
+         * @private
+         */
+        _getTransportProtocol: function _getTransportProtocol() {
 
-    // Open the browser to point to the relayed URL.
-    childProcess.spawn('open', ['http://' + $host + ':' + $port]);
+            if (args.s) {
+
+                // Process all HTTPS requests irrespective of certificates!
+                process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+                return require('https')
+
+            }
+
+            return require('http');
+
+        }
+
+    };
+
+    // Okay, here we go!
+    new Relayed();
 
 })('127.0.0.1', 8910);
